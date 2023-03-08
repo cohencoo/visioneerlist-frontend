2e78fc716fa6ce3d473398c7ce8400b51b1fbc75
cohencoo <cohencoombs@outlook.com>
2023-03-08 15:44:06 +1030

    Live test

---
+++ src/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.scss"
+
+const Home: React.FC = () => {
+    return <>
+        <h1 className={styles.h1}>Hello</h1>
+    </>
+}
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css';
-import App from './App';
+import Home from './Home/Home';
+import App from './App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<App />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
