b80b9f18f64a7aabb0e6cc1dfac3aa6d11dadbd4
cohencoo <cohencoombs@outlook.com>
2023-04-09 22:38:59 +0930

    fix useState bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Home from "./Home/Home"
 import Dashboard from "./Dashboard/Dashboard"
+import axios from "axios"
 
 export const AI_ROUTE = "https://visioneerlist-ai.coclub.repl.co"
 export const MAP_ROUTE = "https://visioneerlist-backend.coclub.repl.co/maps/?q="
@@ -8,20 +9,14 @@ export const API_ROUTE = "https://visioneerlist-backend.coclub.repl.co"
 
 function App() {
     const [app, setApp] = useState("/")
-    const [profiles, setProfiles] = useState({}) as any
+    const [profiles, setProfiles] = useState({})
     const query = new URLSearchParams(window.location.search).get("id")
 
     const fetchData = () => {
-        fetch(API_ROUTE + "/api/profiles")
-            .then((res) => res.json())
-            .then((data) => {
-                setProfiles(data)
-                console.log(data)
-            })
-            .catch((err) => {
-                setTimeout(fetchData, 1000)
-                console.error(err)
-            })
+        axios
+            .get(API_ROUTE + "/api/profiles")
+            .then((res: any) => setProfiles(res.data))
+            .catch(() => setTimeout(fetchData, 1000))
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
