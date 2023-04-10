9801ef7942fdedfba281035fd9a0e474b4aceab3
cohencoo <cohencoombs@outlook.com>
2023-04-10 12:56:54 +0930

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from "react"
+import { useEffect, useState } from "react"
 import Home from "./Home/Home"
 import Dashboard from "./Dashboard/Dashboard"
 import axios from "axios"
@@ -10,34 +10,33 @@ export const API_ROUTE = "https://visioneerlist-backend.onrender.com"
 function App() {
     const [app, setApp] = useState("/")
     const [profiles, setProfiles] = useState({})
-    const DataContext = createContext(profiles)
+    const [success, setSuccess] = useState(false)
     const query = new URLSearchParams(window.location.search).get("id")
 
     const fetchData = () => {
+        if (success) return
+        console.log("Fetching data...")
         axios
-            .get(API_ROUTE + "/api/profiles")
-            .then((res: any) => setProfiles(res.data))
+            .get(API_ROUTE + "/api/profiles?timestamp=" + new Date().getTime())
+            .then((res: any) => {
+                setProfiles(res.data)
+                setSuccess(true)
+            })
             .catch(() => setTimeout(fetchData, 1000))
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => fetchData, [])
+    useEffect(() => fetchData, [profiles])
 
-    return (
-        <>
-            <DataContext.Provider value={profiles}>
-                {app === "/" && !query ? (
-                    <Home profiles={profiles} setApp={setApp} />
-                ) : (
-                    <Dashboard
-                        setProfiles={setProfiles}
-                        viewProfile={query}
-                        DataContext={DataContext}
-                        newListing={app === "/new-profile"}
-                    />
-                )}
-            </DataContext.Provider>
-        </>
+    return app === "/" && !query ? (
+        <Home profiles={profiles} setApp={setApp} />
+    ) : (
+        <Dashboard
+            profiles={profiles}
+            setProfiles={setProfiles}
+            viewProfile={query}
+            newListing={app === "/new-profile"}
+        />
     )
 }
 
